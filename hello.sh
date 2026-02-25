1a9140eed71372d4a32e860d83e7408f34d1d21a
dresky2024 <dre-sky@mail.ru>
2026-02-25 10:50:48 +0500

    Update script: added system info

--- hello.sh
+++ hello.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "DevOps марафон запущен: $(date)"
+USER_NAME="Твое Имя"
+echo "Привет, $USER_NAME!"
+echo "Сегодня: $(date)"
+echo "Моя рабочая директория: $(pwd)"
+echo "Версия ядра Linux: $(uname -r)"
