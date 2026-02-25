502a8b1be1bcebacb2a4958dfcba83f1fceab255
dresky2024 <dre-sky@mail.ru>
2026-02-25 10:55:12 +0500

    Added logging and system user variable

--- hello.sh
+++ hello.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 USER_NAME="Твое Имя"
-echo "Привет, $USER_NAME!"
+echo "Привет, $USER!"
 echo "Сегодня: $(date)"
 echo "Моя рабочая директория: $(pwd)"
 echo "Версия ядра Linux: $(uname -r)"
+echo "Скрипт запущен: $(date)" >> log.txt
